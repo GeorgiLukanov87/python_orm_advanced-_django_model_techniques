9d23a68cd8294bfda95097b7adaabe520156ae1c
Georgi St. Lukanov <goshiii87@gmail.com>
2024-05-20 18:00:59 +0300

    task 1

---
+++ main_app/validators.py
@@ -0,0 +1,15 @@
+import re
+
+from django.core.exceptions import ValidationError
+
+
+# Task 1
+def validate_name(value):
+    for ch in value:
+        if not (ch.isalpha() or ch.isspace()):
+            raise ValidationError('Name can only contain letters and spaces')
+
+
+def validate_phone_number(value):
+    if not re.match(r'\+359\d{9$}', value):
+        raise ValidationError("Phone number must start with a '+359' followed by 9 digits")
