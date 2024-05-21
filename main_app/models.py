7c98ffc0a6ca95a6e93e02b336a99b45a5f7a508
Georgi St. Lukanov <goshiii87@gmail.com>
2024-05-21 15:36:25 +0300

    task 4

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from _decimal import Decimal
 from django.core.validators import MinValueValidator, MinLengthValidator
 from django.db import models
 
+from main_app.mixins import RechargeEnergyMixin
 from main_app.validators import validate_name, validate_phone_number
 
 
@@ -143,4 +144,43 @@ class DiscountedProduct(Product):
     def format_product_name(self) -> str:
         return f"Discounted Product: {self.name}"
 
+
 # Task 4
+class Hero(models.Model, RechargeEnergyMixin):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    hero_title = models.CharField(
+        max_length=100,
+    )
+
+    energy = models.PositiveIntegerField()
+
+
+class SpiderHero(Hero):
+    class Meta:
+        proxy = True
+
+    def swing_from_buildings(self) -> str:
+        self.energy -= 80
+
+        if self.energy > 0:
+            self.save()
+            return f"{self.name} as Spider Hero swings from buildings using web shooters"
+
+        return f"{self.name} as Spider Hero is out of web shooter fluid"
+
+
+class FlashHero(Hero):
+    class Meta:
+        proxy = True
+
+    def run_at_super_speed(self) -> str:
+        self.energy -= 65
+
+        if self.energy > 0:
+            self.save()
+            return f"{self.name} as Flash Hero runs at lightning speed, saving the day"
+
+        return f"{self.name} as Flash Hero needs to recharge the speed force"
